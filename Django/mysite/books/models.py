6e8ba865530289778b51ff7ed0ea58dad9faa096
spxcds <jvbei@live.com>
2016-03-24 21:14:20 +0800

    Modules in Django

--- Django/mysite/books/models.py
+++ Django/mysite/books/models.py
@@ -1,5 +1,38 @@
-from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+class User (models.Model):
+    name = models.CharField(max_length=20)
+    pwd = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    city = models.CharField(max_length=60)
+    state_province = models.CharField(max_length=30)
+    country = models.CharField(max_length=50)
+    website = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    salutation = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+    headshot = models.ImageField(upload_to='/tmp')
+
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher)
+    publication_date = models.DateField()
+
+    def __str__(self):
+        return self.title

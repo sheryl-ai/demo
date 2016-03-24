c3b1b5410ca245d7da2fc0e754d29df5607e38b0
spxcds <jvbei@live.com>
2016-03-25 01:13:33 +0800

    The operation of models in Django

--- Django/mysite/books/models.py
+++ Django/mysite/books/models.py
@@ -7,6 +7,9 @@ class User (models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ['name']
+
 class Publisher(models.Model):
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
@@ -18,21 +21,32 @@ class Publisher(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        ordering = ['name']
+
 class Author(models.Model):
     salutation = models.CharField(max_length=10)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
     email = models.EmailField()
-    headshot = models.ImageField(upload_to='/tmp')
+    #headshot = models.ImageField(upload_to='/tmp')
 
     def __str__(self):
         return '%s %s' % (self.first_name, self.last_name)
+
+    class Meta:
+        ordering = ['first_name']
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
     publication_date = models.DateField()
+    #num_pages = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.title
+    
+    class Meta:
+        ordering = ['title']
+

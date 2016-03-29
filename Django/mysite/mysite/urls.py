ca353fad1bc3ac5101ca22f4fa5514e03a0c657b
spxcds <jvbei@live.com>
2016-03-29 17:05:49 +0800

    I don't know what I have changed

--- Django/mysite/mysite/urls.py
+++ Django/mysite/mysite/urls.py
@@ -5,6 +5,7 @@ from mysite.views import *
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^display_meta/', display_meta),
     url(r'^$', homepage),
     url(r'^time/$', cur_time),
     url(r'^time/plus/(\d{1,5})/$', cur_time_ahead),
--- Django/mysite/mysite/views.py
+++ Django/mysite/mysite/views.py
@@ -2,6 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 import datetime
 
+def display_meta(request):
+    values = request.META.items()
+    values.sort()
+    html = []
+    
+    for k, v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+
+    return HttpResponse('<table>%s</table>' % '\n'.join(html))
+
 def homepage(request):
     return render_to_response('base.html')
 

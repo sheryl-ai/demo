e50d420ca47a124c4dc7e574fcd9345170dff159
spxcds <jvbei@live.com>
2016-04-17 21:11:22 +0800

    修改admin的样式

--- Django/mysite/polls/admin.py
+++ Django/mysite/polls/admin.py
@@ -2,5 +2,26 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 0
+
+class ChoiceTabularInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+            ('Question text',    {'fields': ['question_text']}),
+            ('Date information', {'fields': ['pub_date']}),
+    ]
+
+    search_fields = ['question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    #inlines = [ChoiceInline]
+    inlines = [ChoiceTabularInline]
+
+admin.site.register(Question, QuestionAdmin)
+
 admin.site.register(Choice)
